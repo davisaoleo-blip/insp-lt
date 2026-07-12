b068669d3ecbbfd39233febfadd4b1b5e64ce0d2
davisaoleo-blip <davisaoleo@gmail.com>
2026-07-12 19:54:22 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'insp-lt-v4';
+const ARQUIVOS = ['./', './inspecao-lt-v4.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARQUIVOS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(c => c || fetch(e.request).catch(() => c)));
+});
